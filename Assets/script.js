1e70de895bb45ee91399d43ff0654d3af261f3e8
paulina koch <pjassakoch@gmail.com>
2023-11-30 20:29:03 -0500

    Completed html structure

--- Assets/script.js
+++ Assets/script.js
@@ -1,12 +1,24 @@
 let APIKEY = '1914eaa1075e94091f7d727ffe792791';
 let city;
-let queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=" + inputEl + "&appid=" + APIKEY + "&units=metric" ;
-fetchButton = document.getElementbyId('button');
-fetchButton.setAttribute('style','margin-left: 10px;');
+let dailyForecast = document.getElementById('today-weather');
+fetchButton = document.getElementById('fetch-button');
+
 let inputEl = document.getElementById('user-input')
+//event listener for search button
+fetchButton.addEventListener('click', requestCity);
 
+
+
+//taking in request input
+function requestCity (event) {
+let userInput = inputEl.value.trim();
+if (!userInput) {
+    alert('City Name Required');
+  return
+} 
+event.preventDefault();
+let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&appid=" + APIKEY + "&units=metric";
 console.log(queryURL);
-
 
 //fetch requesat to get  data
 fetch(queryURL)
@@ -17,17 +29,27 @@ fetch(queryURL)
 
 
     //append the data to the html doc
-    then(function(data) {
+    .then(function(data) {
         console.log(data); 
+
     })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
    
     
 
 
 
-    //user input function
-    let userInput = inputEl.value.trim()
-    if (!userInput) {
-        prompt ('City Name Required');
-        return;
-    }
+    
